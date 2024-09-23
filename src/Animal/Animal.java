cb3184e157c9d5e6c76ccd06b4106d2695be95bb
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-23 20:21:16 +0200

    Define Animal interface

---
+++ src/Animal/Animal.java
@@ -0,0 +1,16 @@
+package Animal;
+
+/**
+ * Represents an animal.
+ */
+public interface Animal {
+    /**
+     * Animal makes a sound.
+     */
+    void sound();
+
+    /**
+     * Animal eats.
+     */
+    void eat();
+}
