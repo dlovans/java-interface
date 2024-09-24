bd8a74d3b455a70bb9a2f815df40a2c94f2a91ea
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-24 13:18:27 +0200

    Declare Car class

---
+++ src/Vehicle/Car.java
@@ -0,0 +1,17 @@
+package Vehicle;
+
+/**
+ * Represents a car.
+ */
+public class Car {
+    /**
+     * Car in police chase.
+     */
+    public void move() {
+        System.out.println("Car honks: Beep beeeeeeeeeeep");
+        System.out.println("Car has a speed of 190km/h.");
+        System.out.println("Car pursued by police.");
+        System.out.println("Car makes a hard right and left.");
+        System.out.println("Car crashes...");
+    }
+}
