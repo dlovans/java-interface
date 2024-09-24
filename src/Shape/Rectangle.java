df21e4151f6266670cca6e70b3b69a21e8e0640f
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-24 12:41:38 +0200

    Declare Rectangle class

---
+++ src/Shape/Rectangle.java
@@ -0,0 +1,43 @@
+package Shape;
+
+/**
+ * Represents a rectangle.
+ * Implements Shape.
+ */
+public class Rectangle implements Shape {
+    /**
+     * Width of rectangle.
+     */
+    double width;
+
+    /**
+     * Height of rectangle.
+     */
+    double height;
+
+    /**
+     * Creates an instance of Rectangle.
+     * @param width - Width of rectangle.
+     * @param height - Height of rectangle.
+     */
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    /**
+     * Calculates area of rectangle.
+     * @return - Area of rectangle.
+     */
+    public double area() {
+        return width * height;
+    }
+
+    /**
+     * Calculates perimeter of rectangle.
+     * @return - Perimeter of rectangle.
+     */
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+}
