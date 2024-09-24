9f08e88d4e3c672ef1d6aef62b20c50d6b041657
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-24 12:36:05 +0200

    Test the Animal package content

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import Animal.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    /**
+     * Animal interactions, using Animal package.
+     * @param dog - A dog.
+     * @param cat - A cat.
+     */
+    private void animalInteractions(Animal dog, Animal cat) {
+        dog.sound();
+        cat.sound();
+        dog.eat();
+        cat.eat();
     }
 }
