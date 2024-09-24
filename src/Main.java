261cf1037b1e93e00ddcdab4105bc0931ce5b153
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-24 12:37:10 +0200

    Call method, update access modifier

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import Animal.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        animalInteractions(new Dog(), new Cat());
     }
 
     /**
@@ -10,7 +10,7 @@ public class Main {
      * @param dog - A dog.
      * @param cat - A cat.
      */
-    private void animalInteractions(Animal dog, Animal cat) {
+    private static void animalInteractions(Animal dog, Animal cat) {
         dog.sound();
         cat.sound();
         dog.eat();
