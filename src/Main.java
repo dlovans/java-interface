c3495b86e842eefcfc819378af08166d226e3058
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-24 12:52:38 +0200

    Declare method printing area and perimeter

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import Animal.*;
+import Shape.*;
 
 public class Main {
     public static void main(String[] args) {
         animalInteractions(new Dog(), new Cat());
+        printAreaAndPerimeter(new Rectangle(5, 5), new Circle(5));
     }
 
     /**
@@ -15,5 +17,17 @@ public class Main {
         cat.sound();
         dog.eat();
         cat.eat();
+    }
+
+    /**
+     * Prints area of shapes.
+     * @param rectangle - A rectangle.
+     * @param circle - A circle.
+     */
+    private static void printAreaAndPerimeter(Rectangle rectangle, Circle circle) {
+        System.out.printf("%.2f\n", rectangle.area());
+        System.out.printf("%.2f\n", rectangle.perimeter());
+        System.out.printf("%.2f\n", circle.area());
+        System.out.printf("%.2f\n", circle.perimeter());
     }
 }
