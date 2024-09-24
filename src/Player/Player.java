4d2709e331ae1cc65cbcb97563dfc6d86ef08e7f
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-24 13:28:36 +0200

    Declare Player interface

---
+++ src/Player/Player.java
@@ -0,0 +1,21 @@
+package Player;
+
+/**
+ * Represents a media player.
+ */
+public interface Player {
+    /**
+     * Plays media.
+     */
+    void play();
+
+    /**
+     * Pauses media.
+     */
+    void pause();
+
+    /**
+     * Stops media.
+     */
+    void stop();
+}
